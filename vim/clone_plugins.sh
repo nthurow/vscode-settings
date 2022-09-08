99038e3cf8373e74f346c337660d049897cad204
Nick Thurow <nthurow@gmail.com>
2022-09-08 11:22:20 -0500

    add fugitive to plugins

--- vim/clone_plugins.sh
+++ vim/clone_plugins.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.vim/pack/plugins/start/
 git clone git@github.com:preservim/nerdtree.git ~/.vim/pack/plugins/start/nerdtree
 git clone git@github.com:ctrlpvim/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlp.vim
 git clone git@github.com:tpope/vim-surround.git ~/.vim/pack/plugins/start/vim-surround
+git clone git@github.com:tomasiser/vim-code-dark.git ~/.vim/pack/plugins/start/vim-code-dark
 
 # JavaScript support
 git clone git@github.com:pangloss/vim-javascript.git ~/.vim/pack/plugins/start/vim-javascript
@@ -12,8 +13,9 @@ git clone git@github.com:leafgarland/typescript-vim.git ~/.vim/pack/plugins/star
 git clone git@github.com:maxmellon/vim-jsx-pretty.git ~/.vim/pack/plugins/start/vim-jsx-pretty
 
 git clone --branch release git@github.com:neoclide/coc.nvim.git ~/.vim/pack/plugins/start/coc.nvim
-cd ~/.vim/pack/plugins/start/coc.nvim
+cd ~/.vim/pack/plugins/start/
 vim -c "helptags coc.nvim/doc/ | q"
 
-git clone git@github.com:tomasiser/vim-code-dark.git ~/.vim/pack/plugins/start/vim-code-dark
-
+git clone https://tpope.io/vim/fugitive.git
+cd ~/.vim/pack/plugins/start/
+vim -u NONE -c "helptags fugitive/doc" -c q
